389575ce875102b562e339767e7b4dba1983c79d
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-11-27 20:27:52 +0200

    feat: modify cookies project and add elephant sql

---
+++ cookie_stands/views.py
@@ -0,0 +1,18 @@
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .models import CookieStand
+from .permissions import IsOwnerOrReadOnly
+from .serializers import cookieSerializer
+
+
+class CookieStandList(ListCreateAPIView):
+    queryset = CookieStand.objects.all()
+    serializer_class = cookieSerializer
+
+
+class CookieStandDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = CookieStand.objects.all()
+    serializer_class = cookieSerializer
